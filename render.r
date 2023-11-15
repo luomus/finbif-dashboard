755576c1c049b42bb51719f55e69fe06b2a59718
wkmor1 <willi@mmorris.email>
2023-11-15 09:27:00 +0200

    Only use plausible in production

--- render.r
+++ render.r
@@ -1,5 +1,19 @@
 #!/usr/bin/env r
 
+if (Sys.getenv("BRANCH") == "main") {
+
+    cat(
+      sprintf(
+        '<script defer data-domain="%s" src="%s"></script>\n',
+        Sys.getenv("HOST"),
+        "https://plausible.io/js/script.js"
+      ),
+      file = "plausible.html",
+      append = TRUE
+    )
+
+}
+
 rmarkdown::run(
   "index.Rmd",
   shiny_args = list(port = 3838, host = "0.0.0.0"),

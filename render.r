7a6923f57d642b1d0f0c5052fd1b5ab7f31bc8a2
wkmor1 <willi@mmorris.email>
2024-01-09 16:45:47 +0200

    Reduce noise in logs

--- render.r
+++ render.r
@@ -16,6 +16,6 @@ if (Sys.getenv("BRANCH") == "main") {
 
 rmarkdown::run(
   "index.Rmd",
-  shiny_args = list(port = 3838, host = "0.0.0.0"),
+  shiny_args = list(port = 3838, host = "0.0.0.0", quiet = TRUE),
   render_args = list(quiet = TRUE)
 )

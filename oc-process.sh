fd47f1d97ee7b0bad36d9c83eff3074f80083f6e
wkmor1 <willi@mmorris.email>
2024-07-04 13:59:09 +0300

    Update oc process script

--- oc-process.sh
+++ oc-process.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 i="all"
+f="template.yml"
+e=".env"
 
 while getopts ":f:e:i::" flag; do
-case $flag in
-f) f=${OPTARG} ;;
-e) e=${OPTARG} ;;
-i) i=${OPTARG} ;;
-esac
+  case $flag in
+    f) f=${OPTARG} ;;
+    e) e=${OPTARG} ;;
+    i) i=${OPTARG} ;;
+  esac
 done
 
 set -a
@@ -21,70 +23,74 @@ FINBIF_PRIVATE_API="unset"
 
 if [ "$BRANCH" != "main" ]; then
 
-HOST=$HOST_DEV
-FINBIF_PRIVATE_API="dev"
-DB_USER_PASSWORD=$DEV_DB_USER_PASSWORD
-DB_PRIMARY_PASSWORD=$DEV_DB_PRIMARY_PASSWORD
-DB_SUPER_PASSWORD=$DEV_DB_SUPER_PASSWORD
+  HOST=$HOST_DEV
+  FINBIF_PRIVATE_API="dev"
+  DB_PASSWORD=$DB_PASSWORD_DEV
 
 fi
 
 if [ $i = "volume" ]; then
 
-ITEM=".items[0]"
+  ITEM=".items[0]"
 
-elif [ $i = "image" ]; then
+elif [ $i = "config" ]; then
 
-ITEM=".items[1]"
+  ITEM=".items[1]"
 
-elif [ $i = "build" ]; then
+elif [ $i = "secrets" ]; then
 
-ITEM=".items[2]"
+  ITEM=".items[2]"
 
 elif [ $i = "deploy-app" ]; then
 
-ITEM=".items[3]"
+  ITEM=".items[3]"
 
 elif [ $i = "deploy-api" ]; then
 
-ITEM=".items[4]"
+  ITEM=".items[4]"
 
 elif [ $i = "deploy-db" ]; then
 
-ITEM=".items[5]"
+  ITEM=".items[5]"
 
 elif [ $i = "service-app" ]; then
 
-ITEM=".items[6]"
+  ITEM=".items[6]"
 
 elif [ $i = "service-api" ]; then
 
-ITEM=".items[7]"
+  ITEM=".items[7]"
 
 elif [ $i = "service-db" ]; then
 
-ITEM=".items[8]"
+  ITEM=".items[8]"
 
 elif [ $i = "route" ]; then
 
-ITEM=".items[9]"
+  ITEM=".items[9]"
 
-else
+elif [ $i = "all" ]; then
 
   ITEM=""
 
+else
+
+  echo "Object not found"
+  exit 1
+
 fi
 
+DB_PASSWORD=$(echo -n $DB_PASSWORD | base64)
+
+echo "# $(oc project finbif-dashboard)"
+
 oc process -f $f \
--p BRANCH=$BRANCH \
--p HOST=$HOST \
--p FINBIF_PRIVATE_API=$FINBIF_PRIVATE_API \
--p DB_NAME=$DB_NAME \
--p DB_USER=$DB_USER \
--p DB_PORT=$DB_PORT \
--p DB_PRIMARY_USER=$DB_PRIMARY_USER \
--p DB_SUPER_USER=$DB_SUPER_USER \
--p DB_USER_PASSWORD=$DB_USER_PASSWORD \
--p DB_PRIMARY_PASSWORD=$DB_PRIMARY_PASSWORD \
--p DB_SUPER_PASSWORD=$DB_SUPER_PASSWORD \
-| jq $ITEM
+  -p BRANCH="$BRANCH" \
+  -p HOST="$HOST" \
+  -p FINBIF_PRIVATE_API="$FINBIF_PRIVATE_API" \
+  -p DB_PASSWORD="$DB_PASSWORD" \
+  -p SMTP_SERVER="$SMTP_SERVER" \
+  -p SMTP_PORT="$SMTP_PORT" \
+  -p ERROR_EMAIL_TO="$ERROR_EMAIL_TO" \
+  -p ERROR_EMAIL_FROM="$ERROR_EMAIL_FROM" \
+  | jq $ITEM

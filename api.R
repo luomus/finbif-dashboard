521d6ce4af922aeeeb35a8b4171399552ca91000
wkmor1 <willi@mmorris.email>
2026-06-27 14:03:32 +0300

    Use all collections

--- api.R
+++ api.R
@@ -24,7 +24,8 @@ options(
   finbif_rate_limit = Inf,
   finbif_max_page_size = 3000L,
   finbif_use_async = FALSE,
-  finbif_use_dwc = FALSE
+  finbif_use_dwc = FALSE,
+  finbif_use_all_collections = TRUE
 )
 
 op <- options()
